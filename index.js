060523e257f4aa192ce7b7b16109bee939b45987
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-12 23:27:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function testFunc() {
   return "hi"
 }
 
+const object = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+
 function superbowlWin(Array) {
-roommates.find( function(s) { return s === "winston" })
+objects.find( function(s) { return s === "winston" })
 }
