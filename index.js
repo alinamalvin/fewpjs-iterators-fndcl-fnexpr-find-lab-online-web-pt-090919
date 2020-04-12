83d477189aeea4385223ae9dee3b3a32d0e1d889
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-12 23:36:55 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (arr) => {
+  let year;
+  arr.find(obj => {
+    if (obj["result"] === "W") {
+      year = obj.year;
+    }
+  })
+  return year;
+}
